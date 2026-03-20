3b119bda554b84f840e4a78663fc79ef02db8492
David Da Silva <daviddasilva_dds@yahoo.fr>
2026-03-20 19:25:41 +0100

    intialisating dbt

--- compte_b/src/compte_b/defs/resources.py
+++ compte_b/src/compte_b/defs/resources.py
@@ -1,10 +1,11 @@
-from __future__ import annotations
 
 import os
 from pathlib import Path
 
 import dagster as dg
 from dagster_duckdb import DuckDBResource
+from dagster_dbt import DbtCliResource
+from compte_b.defs.project import dbt_project
 
 
 DB_ENV_VAR = "UNIVERSITY_DUCKDB_PATH"
@@ -20,7 +21,16 @@ def _get_db_path() -> str:
 
 database_resource = DuckDBResource(database=_get_db_path())
 
+dbt_resource = DbtCliResource(
+    project_dir=dbt_project
+)
+
 @dg.definitions
 def resources() -> dg.Definitions:
-    return dg.Definitions(resources={"database": database_resource})
+    return dg.Definitions(
+        resources={
+            "database": database_resource,
+            "dbt": dbt_resource
+        }
+    )
 
